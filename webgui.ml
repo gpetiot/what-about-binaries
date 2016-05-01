0398dab01c23f1c2e508d176914ebc2edec7d1a8
Guillaume Petiot <g.petiot@mail.com>
2016-05-01 21:20:57 +0200

    add license in webgui file

--- webgui.ml
+++ webgui.ml
@@ -1,3 +1,20 @@
+(**************************************************************************)
+(*                                                                        *)
+(*  This file is part of OBAF.                                            *)
+(*                                                                        *)
+(*  You can redistribute it and/or modify it under the terms of the GNU   *)
+(*  Lesser General Public License as published by the Free Software       *)
+(*  Foundation, version 3.0.                                              *)
+(*                                                                        *)
+(*  It is distributed in the hope that it will be useful,                 *)
+(*  but WITHOUT ANY WARRANTY; without even the implied warranty of        *)
+(*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *)
+(*  GNU Lesser General Public License for more details.                   *)
+(*                                                                        *)
+(*  See the GNU Lesser General Public License version 3.0                 *)
+(*  for more details (enclosed in the file LICENSE).                      *)
+(*                                                                        *)
+(**************************************************************************)
 
 let onload _ =
   let document = Dom_html.window##document in
