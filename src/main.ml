d15399df04ea26598f8223ce9da2d4fd9291dae9
Guillaume Petiot <g.petiot@mail.com>
2016-05-10 10:03:34 +0200

    + dynamic symbol table

--- src/main.ml
+++ src/main.ml
@@ -26,21 +26,43 @@ let () =
 	let module A = (val (Archi.addr eclass)) in
 	let module E = (val (Endian.endianness edata)) in
 	let module FH = Elf_header.Make(A)(E) in
+	let pp_sep fmt () = Format.fprintf fmt "" in
+
+	(* file header *)
 	let fh = FH.parse filename in
+	Format.printf "file header:\n%a" FH.pretty fh;
+
+	(* section header *)
 	let shl = FH.Sh.parse fh filename in
+	Format.printf
+	  "\nsection header:\n%a"
+	  (Format.pp_print_list ~pp_sep FH.Sh.pretty) shl;
+
+	(* program header *)
 	let phl = FH.Ph.parse fh filename in
+	Format.printf
+	  "\nprogram header:\n%a"
+	  (Format.pp_print_list ~pp_sep FH.Ph.pretty) phl;
+
+	(* symbol table *)
 	let strtab = FH.Sh.strtab ~filename ~tablename:".strtab" shl in
 	let symtab =
 	  FH.Symtbl.parse ~filename ~tablename:".symtab" ~strtab shl in
+	Format.printf
+	  "\nsymbol table:\n%a"
+	  (Format.pp_print_list ~pp_sep FH.Symtbl.pretty) symtab;
+	
+	(* dynamic symbol table *)
 	begin
-	  Format.printf "file header:\n%a" FH.pretty fh;
-	  let pp_sep fmt () = Format.fprintf fmt "" in
-	  Format.printf "\nsection header:\n%a"
-			(Format.pp_print_list ~pp_sep FH.Sh.pretty) shl;
-	  Format.printf "\nprogram header:\n%a"
-			(Format.pp_print_list ~pp_sep FH.Ph.pretty) phl;
-	  Format.printf "\nsymbol table:\n%a"
-			(Format.pp_print_list ~pp_sep FH.Symtbl.pretty) symtab
+	  try
+	    let dstrtab = FH.Sh.strtab ~filename ~tablename:".dynstr" shl in
+	    let dsymtab =
+	      FH.Symtbl.parse ~filename ~tablename:".dynsym" ~strtab:dstrtab shl
+	    in
+	    Format.printf
+	      "\ndynamic symbol table:\n%a"
+	      (Format.pp_print_list ~pp_sep FH.Symtbl.pretty) dsymtab
+	  with Not_found -> ()
 	end
       with
 	Elf_header.Invalid_Elf -> Format.printf "invalid ELF file !\n"
