a9cccecf8e1044419cf3c70347c6aaa5ca470ab6
Guillaume Petiot <g.petiot@mail.com>
2016-10-14 00:06:10 +0200

    less available module fields

--- src/archi.mli
+++ src/archi.mli
@@ -9,11 +9,6 @@ sig
   val eclass : eclass
   val size : int
   val header_size : int
-  val word_size : int
-  val addr_size : int
-  val half_size : int
-  val xword_size : int
-  val off_size : int
   val ph_offsets : int * int * int * int * int * int * int * int
   val ph_sizes : int * int * int * int * int * int * int * int
   val sym_offsets : int * int * int * int * int * int
