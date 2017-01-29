a0eaf8eef0ee6bb04fc8c698611deafb5ad0e028
Guillaume Petiot <g.petiot@mail.com>
2017-01-29 10:35:58 +0100

    decode x86/x86_64 instructions

--- src/machine.ml
+++ src/machine.ml
@@ -13,6 +13,7 @@ type emachine =
 ;;
 
 type t = emachine;;
+type instr = Capstone.cs_insn0;;
 
 let of_int i =
   if i = int_of_string "0x00" then None
@@ -42,3 +43,53 @@ let to_string = function
 ;;
 
 let pretty fmt x = Format.fprintf fmt "%s" (to_string x);;
+
+
+module type Instr =
+sig
+  val decode : Capstone.mode list -> string -> instr list
+end;;
+
+
+let pretty_instr fmt insn =
+  Format.fprintf fmt "0x%x\t%s\t%s\n"
+    insn.Capstone.address insn.Capstone.mnemonic insn.Capstone.op_str;
+;;
+
+module Arm_Instr : Instr =
+struct
+  let arch = Capstone.CS_ARCH_ARM
+
+  let decode _ _ = [];;
+end;;
+
+module X86_Instr : Instr =
+struct
+  let arch = Capstone.CS_ARCH_X86
+    
+  let decode modes str =
+    let handle = Capstone.cs_open arch modes in
+    let _=
+      Capstone.cs_option handle Capstone.CS_OPT_DETAIL Capstone._CS_OPT_ON in
+    let instr = Capstone.cs_disasm handle str 0x1000L 0L in
+    (match Capstone.cs_close handle with
+    | 0 -> ()
+    | _ -> failwith "Failed to close handle");
+    instr
+  ;;
+;;
+end;;
+
+let instr x = match x with
+  | ARM -> (module Arm_Instr : Instr)
+  | X86
+  | X86_64 -> (module X86_Instr : Instr)
+  | None
+  | SPARC
+  | MIPS
+  | PowerPC
+  | SuperH
+  | IA64
+  | AArch64 ->
+     failwith (Format.sprintf "No instruction set for %s" (to_string x))
+;;
--- src/machine.mli
+++ src/machine.mli
@@ -3,3 +3,13 @@ type t;;
 
 val of_int: int -> t;;
 val pretty: Format.formatter -> t -> unit;;
+
+type instr;;
+val pretty_instr : Format.formatter -> instr -> unit;;
+  
+module type Instr =
+sig
+  val decode : Capstone.mode list -> string -> instr list
+end;;
+
+val instr: t -> (module Instr);;
